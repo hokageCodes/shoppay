c21cbc3a79f97e1803567f3826e3742e087f568c
hokageCodes <hashsenju94@gmail.com>
2023-03-14 20:12:53 +0100

    forgrot password attempt

--- pages/api/auth/forgot.js
+++ pages/api/auth/forgot.js
@@ -1,91 +1,33 @@
-import styles from "../../styles/forgot.module.scss";
-import Header from "../../../components/header";
-import Footer from "../../../components/footer";
-import { BiLeftArrowAlt } from "react-icons/bi";
-import CircledIconBtn from "../../../components/buttons/circledIconBtn";
-import LoginInput from "../../components/inputs/loginInput";
-import { useState } from "react";
-import { Formik, Form } from "formik";
-import * as Yup from "yup";
-import Link from "next/link";
-import DotLoaderSpinner from "../../../components/loaders/dotLoader";
-import axios from "axios";
-import { getSession } from "next-auth/react";
+import nc from "next-connect";
+import bcrypt from "bcrypt";
+import { validateEmail } from "../../../utils/validation";
+import db from "../../../utils/db";
+import User from "../../../models/User";
+import { createActivationToken, createResetToken } from "../../../utils/tokens";
+import { sendEmail } from "../../../utils/sendEmails";
+import { resetEmailTemplate } from "../../../emails/resetEmailTemplate";
+const handler = nc();
 
-
-export default function forgot() {
-  const [email, setEmail] = useState("");
-  const [error, setError] = useState("");
-  const [success, setSuccess] = useState("");
-  const [loading, setLoading] = useState("");
-  const emailValidation = Yup.object({
-    email: Yup.string()
-      .required(
-        "You'll need this when you log in and if you ever need to reset your password."
-      )
-      .email("Enter a valid email address."),
-  });
-  const forgotHandler = async () => {
-    try {
-      setLoading(true);
-      const { data } = await axios.post("/api/auth/forgot", {
-        email,
-      });
-      setError("");
-      setSuccess(data.message);
-      setLoading(false);
-      setEmail("");
-    } catch (error) {
-      setLoading(false);
-      setSuccess("");
-      setError(error.response.data.message);
+handler.post(async (req, res) => {
+  try {
+    await db.connectDb();
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "This email does not exist." });
     }
-  };
-  return (
-    <>
-      {loading && <DotLoaderSpinner loading={loading} />}
-      <Header country="" />
-      <div className={styles.forgot}>
-        <div>
-          <div className={styles.forgot__header}>
-            <div className={styles.back__svg}>
-              <BiLeftArrowAlt />
-            </div>
-            <span>
-              Forgot your password ? <Link href="/">Login instead</Link>
-            </span>
-          </div>
-          <Formik
-            enableReinitialize
-            initialValues={{
-              email,
-            }}
-            validationSchema={emailValidation}
-            onSubmit={() => {
-              forgotHandler();
-            }}
-          >
-            {(form) => (
-              <Form>
-                <LoginInput
-                  type="text"
-                  name="email"
-                  icon="email"
-                  placeholder="Email Address"
-                  onChange={(e) => setEmail(e.target.value)}
-                />
+    const user_id = createResetToken({
+      id: user._id.toString(),
+    });
+    const url = `${process.env.BASE_URL}/auth/reset/${user_id}`;
+    sendEmail(email, url, "", "Reset your password.", resetEmailTemplate);
+    await db.disconnectDb();
+    res.json({
+      message: "An email has been sent to you, use it to reset your password.",
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-                <CircledIconBtn type="submit" text="Send link" />
-                <div style={{ marginTop: "10px" }}>
-                  {error && <span className={styles.error}>{error}</span>}
-                  {success && <span className={styles.success}>{success}</span>}
-                </div>
-              </Form>
-            )}
-          </Formik>
-        </div>
-      </div>
-      <Footer country="" />
-    </>
-  );
-}
+export default handler;
