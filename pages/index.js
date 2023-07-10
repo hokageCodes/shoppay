6ad77e1673f838b5365bbabac4f81fdd6611cd16
hokageCodes <hashsenju94@gmail.com>
2023-07-10 04:33:16 +0100

    working on categories section

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import Footer from '../components/footer'
 import axios from 'axios'
 import Main from '@/components/home/main/main'
 import FlashDeals from '@/components/home/flashDeals'
+import Category from '@/components/home/category'
 
 
 export default function Home({ country }) {
@@ -17,6 +18,32 @@ export default function Home({ country }) {
           <div className={styles.container}>
             <Main />
             <FlashDeals />
+            <div className={styles.home__category}>
+              <Category
+                header="Dresses"
+                products={women_dresses}
+                background="#5a31f4"
+              />
+              {!isMedium && (
+                <Category
+                  header="Shoes"
+                  products={women_shoes}
+                  background="#3c811f"
+                />
+              )}
+              {isMobile && ( 
+                <Category
+                  header="Shoes"
+                  products={women_shoes}
+                  background="#3c811f"
+                />
+              )}
+              <Category
+                header="Accessories"
+                products={women_accessories}
+                background="#000"
+              />
+            </div>
           </div>
         </div>
       <Footer country={country}/>
